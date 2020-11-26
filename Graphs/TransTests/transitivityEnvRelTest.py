27a671ee31c67aec63e30bc8f7a49f8bdfa23a4a
Jack Pottage <jack.pottage@gmail.com>
2020-11-26 16:43:06 +0000

    Added some tests on transitivity.

---
+++ Graphs/TransTests/transitivityEnvRelTest.py
@@ -0,0 +1,54 @@
+import transitiveXAI
+import transitiveCXAI
+from matplotlib import pyplot as plt
+import numpy as np
+theTruth = "12345" #list(string.ascii_lowercase)
+timeArrayT = []
+timeArrayC = []
+loops = 20
+for k in range(loops):
+    print(k)
+    environmentReliability = 0.8+(k+1)/loops*0.2
+    timeArrayAvgT = []
+    timeArrayAvgC = []
+    subloops = 10
+    for j in range(subloops):
+        agentReliability = 0.9
+        agentArrayT = transitiveXAI.genAgents(50,agentReliability)
+        agentArrayC = transitiveCXAI.genAgents(50,agentReliability)
+        counter  = 0
+        continueLooping = True
+        while continueLooping == True:
+            guessAccuracyT = 0
+            guessAccuracyC = 0
+            for i in range(len(agentArrayT)):
+                agentArrayT[i] = transitiveXAI.agentAction(agentArrayT[i], environmentReliability, agentArrayT, theTruth, 0.5, 0.1)
+                guessAccuracyT += transitiveXAI.checkAgentGuessAccuracy(agentArrayT[i][4],theTruth)/len(agentArrayT)
+                agentArrayC[i] = transitiveCXAI.agentAction(agentArrayC[i], environmentReliability, agentArrayC, theTruth, 0.5, 0.1)
+                guessAccuracyC += transitiveCXAI.checkAgentGuessAccuracy(agentArrayC[i][4],theTruth)/len(agentArrayC)
+            counter+=1
+            if guessAccuracyT>0.75 and len(timeArrayAvgT)==j:
+                timeArrayAvgT.append(counter)
+            if guessAccuracyC>0.75 and len(timeArrayAvgC)==j:
+                timeArrayAvgC.append(counter)
+            if len(timeArrayAvgT+timeArrayAvgC) == 2*(j+1):
+                continueLooping = False
+            if counter >1000:
+                if len(timeArrayAvgT)==j:
+                    timeArrayAvgT.append(counter)
+                if len(timeArrayAvgC)==j:
+                    timeArrayAvgC.append(counter)
+                continueLooping = False
+    timeArrayT.append(sum(timeArrayAvgT)/subloops)
+    timeArrayC.append(sum(timeArrayAvgC)/subloops)
+    # for i in agentArray:
+    #     print(i)
+firstNo = 1/loops*0.2+0.8
+plt.plot(np.linspace(firstNo,1,loops),timeArrayT)
+plt.plot(np.linspace(firstNo,1,loops),timeArrayC)
+plt.legend(["Transitive","No Transitivity"])
+plt.xlabel("Environmental Reliability")
+plt.ylabel("Number of Iterations to Convergence")
+plt.title("Graph comparing performance of Transitivity against Environmental Reliability")
+plt.savefig("Graphs/TransTests/envTransTestAR90.png")
+plt.show()
