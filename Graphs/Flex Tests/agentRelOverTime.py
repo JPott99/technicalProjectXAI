08bab2824b7f4cc3e40203f26e9dda0a0df24a78
Jack Pottage <jack.pottage@gmail.com>
2021-01-13 12:39:19 +0000

    Added some comments that show how to test for convergence.

--- Graphs/Flex Tests/agentRelOverTime.py
+++ Graphs/Flex Tests/agentRelOverTime.py
@@ -1,8 +1,8 @@
 import zealousXAI
 import fickleXAI
-import testerXAI
-import lessTesterXAI
-import zealousTesterXAI
+# import testerXAI
+# import lessTesterXAI
+# import zealousTesterXAI
 import superFickleXAI
 import sys
 from matplotlib import pyplot as plt
@@ -13,7 +13,7 @@ timeArrayF = []
 # timeArrayT = []
 timeArraySF = []
 loops = 20
-reliability = float(sys.argv[1])
+reliability = 1#float(sys.argv[1])
 for k in range(loops):
     agentReliability = 0.5+(k+1)/loops*0.5
     print(k, agentReliability)
@@ -30,6 +30,9 @@ for k in range(loops):
         agentArraySF = superFickleXAI.genAgents(50,agentReliability)
         counter  = 0
         continueLooping = True
+        guessAccuracyZold=[0]
+        guessAccuracyFold=[0]
+        guessAccuracySFold=[0]
         while continueLooping == True:
             guessAccuracyZ = 0
             guessAccuracyF = 0
@@ -45,14 +48,29 @@ for k in range(loops):
                 agentArraySF[i] = superFickleXAI.agentAction(agentArraySF[i], environmentReliability, agentArraySF, theTruth, 0.5, 0.1)
                 guessAccuracySF += superFickleXAI.checkAgentGuessAccuracy(agentArraySF[i][4],theTruth)/len(agentArraySF)
             counter+=1
+            accuracy = 0.0001
+            # if abs(guessAccuracyZ-sum(guessAccuracyZold)/len(guessAccuracyZold))<accuracy and len(timeArrayAvgZ)==j:
+            #     timeArrayAvgZ.append(counter)
+            #     # print("Z =",guessAccuracyZ)
+            # if abs(guessAccuracyF-sum(guessAccuracyFold)/len(guessAccuracyFold))<accuracy and len(timeArrayAvgF)==j:
+            #     timeArrayAvgF.append(counter)
+            #     # print("F =",guessAccuracyF)
+            # # if guessAccuracyT>0.75 and len(timeArrayAvgT)==j:
+            # #     timeArrayAvgT.append(counter)
+            # if abs(guessAccuracySF-sum(guessAccuracySFold)/len(guessAccuracySFold))<accuracy and len(timeArrayAvgSF)==j:
+            #     timeArrayAvgSF.append(counter)
+            #     # print("SF =",guessAccuracySF)
             if guessAccuracyZ>0.75 and len(timeArrayAvgZ)==j:
                 timeArrayAvgZ.append(counter)
             if guessAccuracyF>0.75 and len(timeArrayAvgF)==j:
                 timeArrayAvgF.append(counter)
-            # if guessAccuracyT>0.75 and len(timeArrayAvgT)==j:
-            #     timeArrayAvgT.append(counter)
-            if guessAccuracySF>0.75 and len(timeArrayAvgSF)==j:
-                timeArrayAvgSF.append(counter)
+            if agentReliability < 0.93:
+                if len(timeArrayAvgSF) == j:
+                    timeArrayAvgSF.append(1001)
+            else:
+                if guessAccuracySF>0.75 and len(timeArrayAvgSF)==j:
+                    timeArrayAvgSF.append(counter)
+            # print(timeArrayAvgSF)
             if len(timeArrayAvgZ+timeArrayAvgF+timeArrayAvgSF) == 3*(j+1):
                 continueLooping = False
             if counter >1000:
@@ -65,6 +83,16 @@ for k in range(loops):
                 if len(timeArrayAvgSF)==j:
                     timeArrayAvgSF.append(counter)
                 continueLooping = False
+            guessAccuracyZold.append(guessAccuracyZ)
+            # if len(guessAccuracyZold)>3:
+            #     guessAccuracyZold.pop(0)
+            # guessAccuracyFold.append(guessAccuracyF)
+            # if len(guessAccuracyFold)>3:
+            #     guessAccuracyFold.pop(0)
+            # guessAccuracySFold.append(guessAccuracySF)
+            # if len(guessAccuracySFold)>3:
+            #     guessAccuracySFold.pop(0)
+
     timeArrayZ.append(sum(timeArrayAvgZ)/subloops)
     timeArrayF.append(sum(timeArrayAvgF)/subloops)
     #timeArrayT.append(sum(timeArrayAvgT)/subloops)
